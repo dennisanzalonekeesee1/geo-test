e5888ccd87f3c19f069b789de7c3b3b41b74bb53
dennisanzalonekeesee1 <dennisanzalonekeesee@gmail.com>
2026-03-23 18:23:01 -0700

    added plotly graph

--- geo_app.py
+++ geo_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import random
 import plotly.express as px
+import plotly.graph_objects as go
 
 # 1. Access the password from secrets
 VALID_PASSWORD = st.secrets["auth"]["password"]
